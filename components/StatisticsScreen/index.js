bf080d1d48e566c5f6a925c8e2166549bc57474d
Zaray <cor191025@uvg.edu.gt>
2021-10-02 12:43:53 -0600

    line charts with using the API data

--- components/StatisticsScreen/index.js
+++ components/StatisticsScreen/index.js
@@ -2,66 +2,101 @@ import {Text, View, StyleSheet, Button} from "react-native";
 import { useState, useEffect } from 'react';
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 import Axios from 'axios';
 import { TEMPORALDAILY } from '../utils/rutas'
 
-var respuesta = [
-    { quarter: "aa", earnings: 13000 },
-    { quarter: "as", earnings: 16500 },
-    { quarter: "av", earnings: 14250 },
-    { quarter: "aw", earnings: 19000 }
-  ];
+
   export default function StatisticsScreen({ navigation }) {
-    const [datos, setDatos] = useState(
-        {}
-        );
-        const acceptAccountRequest = async () => {
-          try {
-            const { data } = await Axios.get(TEMPORALDAILY,
-              {
-                  headers: { 
-                      'Cookie': '72433e29e5a9aabda23011a87312c9f6=95caff22feff53d170254d75a7038a05',
-                  },
-                  params: {
-                      parameters: "CLOUD_AMT,ALLSKY_SFC_SW_DWN",
-                      community: "RE",
-                      longitude: -90.9587,
-                      latitude: 14.7156,
-                      start: "20201230",
-                      end: "20201231",
-                      format: "JSON",
-                      } ,
-              });
-              /* fechas = Object.keys(data.properties.parameter.CLOUD_AMT)
-              datos = Object.values(data.properties.parameter.CLOUD_AMT)
-              console.log(fechas)
-              console.log(datos) */
-              // TODO Esto hay meterlo en una lista que tiene diccionarios con llaves y valores de igual nombre
-              console.log(data.properties.parameter.CLOUD_AMT)
-              setDatos(data.properties.parameter.CLOUD_AMT)
-              console.log(respuesta)
-          } catch (e) {
-            console.log(e);
-          }
-        };
+
+    const screenWidth = Dimensions.get("window").width;
+    const [datos, setDatos] = useState({
+    x_axis: [],
+    y_axis: []
+    });
+
+    // Consumo de datos del API
+    const acceptAccountRequest = async () => {
+    try {
+    const { data } = await Axios.get(TEMPORALDAILY,
+      {
+          headers: {
+              'Cookie': '72433e29e5a9aabda23011a87312c9f6=95caff22feff53d170254d75a7038a05',
+          },
+          params: {
+              parameters: "CLOUD_AMT,ALLSKY_SFC_SW_DWN",
+              community: "RE",
+              longitude: -90.9587,
+              latitude: 14.7156,
+              start: "20201230",
+              end: "20201231",
+              format: "JSON",
+              } ,
+      });
+    console.log('new')
+    setDatos({
+      x_axis: Object.keys(data.properties.parameter.CLOUD_AMT),
+      y_axis: Object.values(data.properties.parameter.CLOUD_AMT)
+    });
+    console.log(datos)
+    } catch (e) {
+    console.log(e);
+    }
+    };
+
     useEffect(() => {
-        acceptAccountRequest();
+    acceptAccountRequest();
     }, []);
+
+    const data = {
+        labels: datos.x_axis,
+        datasets: [
+            {
+                data: datos.y_axis,
+                color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`,
+                strokeWidth: 2
+            }
+        ],
+        legend: ['Rainy Days']
+    }
+
+    const chartConfig = {
+      backgroundGradientFrom: "#1E2923",
+      backgroundGradientFromOpacity: 0,
+      backgroundGradientTo: "#08130D",
+      backgroundGradientToOpacity: 0.5,
+      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+      strokeWidth: 2, // optional, default 3
+      barPercentage: 0.5,
+      useShadowColorFromDataset: false // optional
+    };
+
     return (
+      <View style={styles.container}>
         <View style={styles.container}>
-            <View style={styles.container}>
-                <VictoryChart width={350} theme={VictoryTheme.material}>
-                <VictoryBar data={respuesta} x="quarter" y="earnings" />
-                </VictoryChart>
-      </View>
-            <Text>This is the Statistics Screen!</Text>
-            <StatusBar style="auto" />
-            <Button
-                onPress={() => acceptAccountRequest()}
-                title="Return"
+            <LineChart
+                data={data}
+                width={screenWidth}
+                height={220}
+                chartConfig={chartConfig}
+                bezier
             />
         </View>
+        <Text>This is the Statistics Screen!</Text>
+        <StatusBar style="auto" />
+        <Button
+            onPress={() => acceptAccountRequest()}
+            title="Return"
+        />
+      </View>
     );
 }
 
