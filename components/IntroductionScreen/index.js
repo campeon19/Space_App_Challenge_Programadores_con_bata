891ef4e326d1c564dee14e7c377a1bd3cb61d176
Méndez <pablomendez778@gmail.com>
2021-10-01 22:54:25 -0600

    Creo que arreglé el error

--- components/IntroductionScreen/index.js
+++ components/IntroductionScreen/index.js
@@ -1,12 +1,22 @@
 import {Text, View, StyleSheet, Button} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 
 export default function IntroductionScreen({ navigation }) {
+    const [message, setMessage] = useState('Loading...')
+
+    useEffect(() => {
+        fetch('https://reactnative.dev/movies.json')
+            .then((response) => response.json())
+            .then((json) => setMessage(json.movies[0].title))
+            .catch((err) => alert(err))
+    }, [])
+
     return (
         <View style={styles.container}>
             <Text>This is the introduction Screen!</Text>
+            <Text>{message}</Text>
             <StatusBar style="auto" />
             <Button
                 onPress={() => navigation.navigate('Daily')}
