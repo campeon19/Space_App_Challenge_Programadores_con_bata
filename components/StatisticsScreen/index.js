c9ab2646df613e2f9f56b83e5bf770e6daa5b7c2
Zaray <cor191025@uvg.edu.gt>
2021-10-03 01:28:07 -0600

    adding dynamic data to the statistics

--- components/StatisticsScreen/index.js
+++ components/StatisticsScreen/index.js
@@ -6,20 +6,21 @@ import {
     LineChart,
 } from "react-native-chart-kit";
 import {Dimensions} from "react-native";
-import Axios from 'axios';
-import {TEMPORALDAILY} from '../utils/rutas'
 import {Rect, Text as TextSVG, Svg, Polygon} from "react-native-svg";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-export default function StatisticsScreen({ navigation }) {
-
+export default function StatisticsScreen({ route, navigation }) {
+    const { location } = route.params;
     const screenWidth = Dimensions.get("window").width;
     const [datos, setDatos] = useState({
-        x_axis: [0],
-        y_axis: [0],
-        labels: [0],
+        x_axis: {a:[0]},
+        y_axis: {a:[0]},
+        labels: {a:[0]},
+        parameters: ['a'],
+        dates: [0, 0],
+        coordinates: [location.geometry.coordinates[0], location.geometry.coordinates[1]],
     });
     const [tooltipPos, setTooltipPos] = useState({
         x: 0,
@@ -28,63 +29,82 @@ export default function StatisticsScreen({ navigation }) {
         value: []
     })
 
-    // Consumo de datos del API
-    const acceptAccountRequest = async () => {
-        try {
-            const {data} = await Axios.get(TEMPORALDAILY,
-                {
-                    headers: {
-                        'Cookie': '72433e29e5a9aabda23011a87312c9f6=95caff22feff53d170254d75a7038a05',
-                    },
-                    params: {
-                        parameters: "CLOUD_AMT,ALLSKY_SFC_SW_DWN",
-                        community: "RE",
-                        longitude: -90.9587,
-                        latitude: 14.7156,
-                        start: "20201201",
-                        end: "20201231",
-                        format: "JSON",
-                    },
-                });
-            const labels = []
-            const formatLabel = []
-            const labelRadix = Math.max(Math.floor(datos.x_axis.length / 4), 1)
-            Object.keys(data.properties.parameter.CLOUD_AMT).map((item, index)=>{
-                const year = item.slice(0, 4)
-                const month = parseInt(item.slice(4, 6), 10) - 1
-                const day = item.slice(6, 9)
-                const date = months[month] + ' ' + day + ' ' + year
-                labels.push(date)
-                if (index% labelRadix === 0)
-                    formatLabel.push(date)
-                else
-                    formatLabel.push('')
-            })
-            await setDatos({
-                ...datos,
-                x_axis: labels,
-                y_axis: Object.values(data.properties.parameter.CLOUD_AMT),
-                labels: formatLabel
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    };
+    const setDate = (dates) =>{
+        const labelRadix = Math.max(Math.floor(dates.length / 4), 1)
+        const date = []
+        const labels = []
+        dates.map((item, index)=> {
+            const year = item.slice(0, 4)
+            const month = parseInt(item.slice(4, 6), 10) - 1
+            const day = item.slice(6, 9)
+            date.push(months[month] + ' ' + day + ' ' + year)
+
+            if (index% labelRadix === 0)
+                labels.push(months[month] + ' ' + day + ' ' + year)
+            else
+                labels.push('')
+        })
+
+        return [date, labels]
+    }
+
+    const setHourDate = (dates) =>{
+        const labelRadix = Math.max(Math.floor(dates.length / 4), 1)
+        console.log(dates.length)
+        const date = []
+        const labels = []
+        dates.map((item, index)=> {
+            const year = item.slice(0, 4)
+            const month = parseInt(item.slice(4, 6), 10) - 1
+            const hour = item.slice(6, 8)
+            const minute = item.slice(8, 11)
+            date.push(hour + ':' + minute + ', ' + months[month] + ' ' + year)
+
+            if (index% labelRadix === 0)
+                labels.push(hour + ':' + minute + ', ' + months[month] + ' ' + year)
+            else
+                labels.push('')
+        })
+
+        return [date, labels]
+    }
 
     useEffect(() => {
-        acceptAccountRequest();
+
+        // Fecha
+        const dates = [location.header.start, location.header.end]
+        const date = setDate(dates)
+
+        // Extracción de data
+        const x_axis = {}
+        const y_axis = {}
+        const formatLabel = {}
+        const parameters = Object.keys(location.properties.parameter)
+        parameters.map((param) =>{
+            const keys = Object.keys(location.properties.parameter[param])
+            const values = Object.values(location.properties.parameter[param])
+            var labels
+            if (dates[0] === dates[1])
+                labels = setHourDate(keys)
+            else
+                labels = setDate(keys)
+
+            x_axis[param] = labels[0]
+            y_axis[param] = values
+            formatLabel[param] = labels[1]
+        })
+        setDatos({
+            ...datos,
+            x_axis: x_axis,
+            y_axis: y_axis,
+            labels: formatLabel,
+            parameters: parameters,
+            dates: date,
+        })
+
+
     }, []);
 
-
-    const data = {
-        labels: datos.labels,
-        datasets: [
-            {
-                data: datos.y_axis,
-            }
-        ],
-        legend: ['Rainy Days']
-    }
 
     const chartConfig = {
         backgroundGradientFrom: '#FFF',
@@ -110,95 +130,114 @@ export default function StatisticsScreen({ navigation }) {
     return (
         <View style={styles.container}>
             <View >
-                <Text style={styles.title}> IDK </Text>
                 <View style={styles.card}>
-                    <Icon name="location-on" size={35} color="#25166B" />
-                    <View style={styles.column}>
-                        <View>
-                            <Text style={styles.text}> Latitude: </Text>
-                            <Text style={styles.text}> -95.523 </Text>
+                    <Icon style={{marginEnd: 10}} name="location-on" size={40} color="#25166B" />
+                    <View style={styles.row}>
+                        <View style={styles.column}>
+                            <Text style={styles.subtitle}> Longitude </Text>
+                            <Text style={styles.text}> {datos.coordinates[0]} </Text>
                         </View>
-                        <View>
-                            <Text style={styles.text}> Latitude: </Text>
-                            <Text style={styles.text}> -95.523 </Text>
+                        <View  style={styles.column}>
+                            <Text style={styles.subtitle}> Latitude </Text>
+                            <Text style={styles.text}> {datos.coordinates[1]} </Text>
                         </View>
                     </View>
                 </View>
                 <View style={styles.card}>
-                    <Icon name="date-range" size={35} color="#25166B" />
-                    <View style={styles.column}>
-                        <Text style={styles.text}> Start: </Text>
-                        <Text style={styles.text}> End: </Text>
+                    <Icon style={{marginEnd: 10}} name="date-range" size={40} color="#25166B" />
+                    <View style={styles.row}>
+                        <View  style={styles.column}>
+                            <Text style={styles.subtitle}> Start </Text>
+                            <Text style={styles.text}> {datos.dates[0][0]} </Text>
+                        </View>
+                        <View  style={styles.column}>
+                            <Text style={styles.subtitle}> End </Text>
+                            <Text style={styles.text}> {datos.dates[0][1]} </Text>
+                        </View>
                     </View>
                 </View>
             </View>
-            <View  style={styles.container}>
-                <LineChart
-                    data={data}
-                    width={screenWidth - 10}
-                    height={330}
-                    chartConfig={chartConfig}
-                    withInnerLines={false}
-                    decorator={() => {
-                        return tooltipPos.visible
-                            ? <View>
-                                <Svg>
-                                    <Rect x={tooltipPos.x - 25}
-                                          y={tooltipPos.y - 30}
-                                          rx={3}
-                                          ry={3}
-                                          width="50"
-                                          height="20"
-                                          fill="rgb(224, 228, 231)"
-                                    />
-                                    <Polygon
-                                        points={`${tooltipPos.x},${tooltipPos.y - 5} ${tooltipPos.x + 5},${tooltipPos.y - 10} ${tooltipPos.x - 5},${tooltipPos.y - 10}`}
-                                        fill="rgb(224, 228, 231)"
+            {
+                datos.parameters.map((param, index) => {
+                    return(
+                        <View  key={index} style={styles.container}>
+                            <LineChart
+                                data={{
+                                    labels: datos.labels[param],
+                                    datasets: [
+                                        {
+                                            data: datos.y_axis[param],
+                                        }
+                                    ],
+                                }}
+                                width={screenWidth - 10}
+                                height={330}
+                                chartConfig={chartConfig}
+                                withInnerLines={false}
+                                decorator={() => {
+                                    return tooltipPos.visible
+                                        ? <View>
+                                            <Svg>
+                                                <Rect x={tooltipPos.x - 25}
+                                                      y={tooltipPos.y - 30}
+                                                      rx={3}
+                                                      ry={3}
+                                                      width="50"
+                                                      height="20"
+                                                      fill="rgb(224, 228, 231)"
+                                                />
+                                                <Polygon
+                                                    points={`${tooltipPos.x},${tooltipPos.y - 5} ${tooltipPos.x + 5},${tooltipPos.y - 10} ${tooltipPos.x - 5},${tooltipPos.y - 10}`}
+                                                    fill="rgb(224, 228, 231)"
 
-                                    />
-                                    <TextSVG
-                                        x={tooltipPos.x}
-                                        y={tooltipPos.y - 22.5}
-                                        fill="black"
-                                        fontSize="7"
-                                        textAnchor="middle">
-                                        {tooltipPos.value[0]}
-                                    </TextSVG>
-                                    <TextSVG
-                                        x={tooltipPos.x}
-                                        y={tooltipPos.y - 13}
-                                        fill="black"
-                                        fontSize="8"
-                                        fontWeight="bold"
-                                        textAnchor="middle">
-                                        {tooltipPos.value[1]}
-                                    </TextSVG>
-                                </Svg>
-                            </View>
-                            : null
-                    }}
-                    onDataPointClick={(data) => {
-                        let isSamePoint = (tooltipPos.x === data.x
-                            && tooltipPos.y === data.y)
-                        let labelIndex = datos.y_axis.indexOf(data.value)
-                        let label = datos.x_axis[labelIndex]
+                                                />
+                                                <TextSVG
+                                                    x={tooltipPos.x}
+                                                    y={tooltipPos.y - 22.5}
+                                                    fill="black"
+                                                    fontSize="7"
+                                                    textAnchor="middle">
+                                                    {tooltipPos.value[0]}
+                                                </TextSVG>
+                                                <TextSVG
+                                                    x={tooltipPos.x}
+                                                    y={tooltipPos.y - 13}
+                                                    fill="black"
+                                                    fontSize="8"
+                                                    fontWeight="bold"
+                                                    textAnchor="middle">
+                                                    {tooltipPos.value[1]}
+                                                </TextSVG>
+                                            </Svg>
+                                        </View>
+                                        : null
+                                }}
+                                onDataPointClick={(data) => {
+                                    let isSamePoint = (tooltipPos.x === data.x
+                                        && tooltipPos.y === data.y)
+                                    let labelIndex = datos.y_axis[param].indexOf(data.value)
+                                    let label = datos.x_axis[param][labelIndex]
 
-                        isSamePoint ? setTooltipPos((previousState) => {
-                                return {
-                                    ...previousState,
-                                    value: [label, data.value],
-                                    visible: !previousState.visible
-                                }
-                            })
-                            : setTooltipPos({ x: data.x, value: [label, data.value], y: data.y, visible: true });
-                    }}
-                    bezier
-                />
-            </View>
+                                    isSamePoint ? setTooltipPos((previousState) => {
+                                            return {
+                                                ...previousState,
+                                                value: [label, data.value],
+                                                visible: !previousState.visible
+                                            }
+                                        })
+                                        : setTooltipPos({ x: data.x, value: [label, data.value], y: data.y, visible: true });
+                                }}
+                                bezier
+                            />
+                        </View>
+                    );
+                })
+            }
+
             <Text>This is the Statistics Screen!</Text>
             <StatusBar style="auto"/>
             <Button
-                onPress={() => acceptAccountRequest()}
+                // onPress={() => acceptAccountRequest()}
                 title="Return"
             />
         </View>
@@ -217,26 +256,36 @@ const styles = StyleSheet.create({
         marginRight: 20,
         marginTop: 10,
         padding:10,
+        width: Dimensions.get("window").width - 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    row:{
+        paddingBottom: 5,
+        paddingEnd: 8,
+        paddingStart: 8,
         borderBottomWidth: 0.5,
         borderBottomColor: '#000',
-        width: Dimensions.get("window").width - 20,
-        flexDirection: 'row'
-    },
-    column:{
-        borderBottomWidth: 0.5,
-        borderBottomColor: '#F5F5F5',
         flexDirection: 'row',
+        width: '80%',
+        justifyContent: 'space-between',
+
+    },
+    column: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     title:{
        color: '#6A706E',
-       width: 100,
        fontSize: 18,
        fontWeight: 'bold'
     },
     subtitle:{
-        color: "#25166B",
-        fontSize: 14,
-        fontWeight: 'bold'
+        color: "#6A706E",
+        fontSize: 18,
+        paddingBottom: 5
     },
 
 });
